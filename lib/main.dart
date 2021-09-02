dd41ec867bbfeebd9aa5df3115eb822d37ccb65c
Gaurav Tantuway <dev.gauravtantuway@gmail.com>
2021-09-03 03:38:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'isntagram_search_tab.dart';
 
 void main() {
